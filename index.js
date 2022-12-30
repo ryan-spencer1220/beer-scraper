0e726e662bf6acb2c67b356f1c5d1c17c5041200
Ryan Spencer <ryan.spencer1220@gmail.com>
2022-12-30 10:14:57 -0800

    remove empty js file

--- index.js
+++ index.js
@@ -6,24 +6,18 @@ async function fetchApex() {
   const page = await browser.newPage();
   await page.goto("https://www.apexbar.com/menu");
 
-  const html = await page.content();
-
-  const tableData = await page.evaluate(() =>
+  const beerData = await page.evaluate(() =>
     Array.from(document.querySelectorAll("td"), (e) => e.innerHTML)
   );
 
   const apexJSON = [];
 
-  for (let i = 0; i < tableData.length; i += 6) {
-    const chunk = tableData.slice(i, i + 6);
+  for (let i = 0; i < beerData.length; i += 6) {
+    const chunk = beerData.slice(i, i + 6);
     apexJSON.push(
       (obj = {
-        beerName: chunk[0],
-        price: chunk[1],
-        brewery: chunk[2],
-        state: chunk[3],
-        alcohol: chunk[4],
-        size: chunk[5],
+        beerName: `${chunk[2]} ${chunk[0]}`,
+        location: "Apex",
       })
     );
   }
@@ -40,33 +34,81 @@ async function fetchBelmontStation() {
     "https://www.taphunter.com/location/belmont-station/6549318358269952"
   );
 
-  const html = await page.content();
-
-  const tableData = await page.evaluate(() =>
+  const beerData = await page.evaluate(() =>
     Array.from(document.querySelectorAll(".media-body a"), (e) => e.innerHTML)
   );
 
-  console.log(tableData);
+  const belmontStationJSON = [];
 
-  // const apexJSON = [];
+  for (let i = 0; i < beerData.length; i++) {
+    const chunk = beerData.slice(i, i + 6);
+    belmontStationJSON.push(
+      (obj = {
+        beerName: chunk[0],
+        location: "Belmont Station",
+      })
+    );
+  }
 
-  // for (let i = 0; i < tableData.length; i += 6) {
-  //   const chunk = tableData.slice(i, i + 6);
-  //   apexJSON.push(
-  //     (obj = {
-  //       beerName: chunk[0],
-  //       price: chunk[1],
-  //       brewery: chunk[2],
-  //       state: chunk[3],
-  //       alcohol: chunk[4],
-  //       size: chunk[5],
-  //     })
-  //   );
-  // }
-
-  // console.log(apexJSON);
+  console.log(belmontStationJSON);
 
   await browser.close();
 }
 
-fetchBelmontStation();
+async function fetchRoscoes() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto("https://www.roscoespdx.com/");
+
+  const beerData = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("."), (e) => e.innerHTML)
+  );
+
+  const roscoesJSON = [];
+
+  // for (let i = 0; i < beerData.length; i++) {
+  //   const chunk = beerData.slice(i, i + 6);
+  //   roscoesJSON.push(
+  //     (obj = {
+  //       beerName: chunk[0],
+  //       location: "Roscoes",
+  //     })
+  //   );
+  // }
+
+  console.log(beerData);
+
+  await browser.close();
+}
+
+async function fetchLoyalLegion() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.taphunter.com/location/belmont-station/6549318358269952"
+  );
+
+  const beerData = await page.evaluate(() =>
+    Array.from(document.querySelectorAll(".media-body a"), (e) => e.innerHTML)
+  );
+
+  const belmontStationJSON = [];
+
+  for (let i = 0; i < beerData.length; i++) {
+    const chunk = beerData.slice(i, i + 6);
+    belmontStationJSON.push(
+      (obj = {
+        beerName: chunk[0],
+        location: "Loyal Legion",
+      })
+    );
+  }
+
+  console.log(belmontStationJSON);
+
+  await browser.close();
+}
+
+// fetchBelmontStation();
+// fetchApex();
+fetchRoscoes();
